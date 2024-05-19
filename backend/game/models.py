4d3cff5a15c12bb8c82d2e7a0589d98ef9d82263
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-05-19 17:06:11 +0900

    feat: Add user, game table model

--- backend/game/models.py
+++ backend/game/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from user.models import CustomUser
 
 # Create your models here.
+class Game(models.Model):
+	GAME_TYPE_CHOICES = [
+		('TYPE1', 'Local Game'),
+		('TYPE2', 'Multi Game'),
+	]
+
+	user1 = models.ForeignKey(CustomUser, related_name='games_as_user1', on_delete=models.CASCADE)
+	user2 = models.ForeignKey(CustomUser, related_name='games_as_user2', on_delete=models.CASCADE)
+	user1_score = models.IntegerField(default=0)
+	user2_score = models.IntegerField(default=0)
+	game_type = models.CharField(max_length=10, choices=GAME_TYPE_CHOICES)
+
+	def __str__(self):
+		return f"Game {self.id}: {self.user1} vs {self.user2}"
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+	nickname = models.CharField(max_length=20, null=True, blank=True)
+	profile_img = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+	status_msg = models.CharField(max_length=40, null=True, blank=True)
+	# language = models.CharField(max_length=20, null=True, blank=True)
+
+	def __str__(self):
+		return self.nickname if self.nickname else self.username
+	
+class Friend(models.Model):
+	user = models.ForeignKey(CustomUser, related_name='friends', on_delete=models.CASCADE)
+	friend = models.ForeignKey(CustomUser, related_name='friend_of', on_delete=models.CASCADE)
+	
+	class Meta:
+		unique_together = ('user', 'friend')
+
+	def __str__(self):
+		return f"{self.user} is friends with {self.friend}"
