996d00116d0ad8373c5839d8be79f97220387e08
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-05-27 16:19:16 +0900

    feat: user 웹소켓은 아직 구현중

--- backend/game/urls.py
+++ backend/game/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("result/", views.GameResultView.as_view(), name="game_result"),
     path("<str:room_name>/", views.room, name="room"),
-    path('result/', views.GameResultView.as_view(), name='game_result'),
 ]
