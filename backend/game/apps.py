ff3d417c8e577cd151a44c110207e1f8f18ed144
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-06-07 16:38:43 +0900

    feat: 서버를 재시작해도 Redis에 방이 삭제가 안되서 서버 재시작할때마다 방에 클라이언트가 없으면 방 삭제하는 코드 구현

--- backend/game/apps.py
+++ backend/game/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
+from django.core.cache import cache
+import redis
 
 
 class GameConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'game'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "game"
+
+    def ready(self):
+        self.clean_up_rooms()
+
+    def clean_up_rooms(self):
+        # Redis 클라이언트 생성
+        redis_client = redis.StrictRedis(host="redis", port=6379, db=0)
+
+        # 'asgi:group:game_room_*' 패턴을 가진 키들을 삭제
+        keys = redis_client.keys("asgi:group:game_room_*")
+        if keys:
+            redis_client.delete(*keys)
+            print(f"Deleted {len(keys)} keys from Redis")
