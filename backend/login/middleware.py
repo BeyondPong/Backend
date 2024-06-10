5a8b77912b277e44c6cf4b78c1b06b7bf918e106
Jihyun Kim <ksk208105@gmail.com>
2024-06-10 21:43:52 +0900

    feat: middleware에서 인식하는 함수 이름으로 변경 및 request.user와 auth에 정보 저장

--- backend/login/middleware.py
+++ backend/login/middleware.py
@@ -3,29 +3,33 @@ import logging
 from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
 from rest_framework import status
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+
+from user.models import Member
 
 # just for debugging
 logger = logging.getLogger(__name__)
 
 
 class JWTAuthenticationMiddleware(MiddlewareMixin):
-    def authorize_request(self, request):
-        logger.debug("============HERE================")
-        # # if api for login: PASS
-        # if request.path == "/login/oauth/":
-        #     return None
-        # # else
+    def process_request(self, request):
+        logger.debug("========== PROCESS REQUEST ==========")
         auth_header = request.headers.get("Authorization")
         if auth_header:
             try:
                 # token: {Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...(=jwt)}
                 jwt_token = auth_header.split(" ")[1]
-                logger.debug(f"=========jwt_token: {jwt_token}=========")
+                logger.debug(f"JWT_token: {jwt_token}")
                 payload = self._decode_jwt(jwt_token)
                 if not payload:
                     return Response({"error": "Invalid jwt-token"}, status=status.HTTP_400_BAD_REQUEST)
+                try:
+                    user = Member.objects.get(nickname=payload["nickname"])
+                    request.user = user
+                    request.auth = jwt_token
+                    logger.debug(f"========= Authenticated user: {user.nickname}=========")
+                except Member.DoesNotExist:
+                    return Response({"error": "User not found"}, status=status.HTTP_400_BAD_REQUEST)
             except IndexError:
                 # error for split-jwt token
                 return Response({"error": "Invalid jwt-token-header(split)"}, status=status.HTTP_400_BAD_REQUEST)
--- backend/login/urls.py
+++ backend/login/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 urlpatterns = [
     path("oauth/", views.OAuth42SocialLogin.as_view(), name="callback"),
+    # test for decode jwt
+    # path("oauth/decode/", views.jwtDecode, name="jwt decode test"),
+
+    # todo two factor requests(send email, verify code)
     # path("two_fa/request/", , name="two_fa_request")
     # path("two_fa/verify/", , name="two_fa_verify")
 ]
