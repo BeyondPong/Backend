f5ced1d2e4b80f091cb6e7816e9bf10763f86176
park hyung jun <55447614+wamma@users.noreply.github.com>
2024-06-13 18:22:32 +0900

    feat: mode가 토너먼트일 때 닉네임 중복 검사 로직 구현

--- backend/game/views.py
+++ backend/game/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from game.serializers import GameResultSerializer
 from .utils import generate_room_name
+from django.core.cache import cache
 
 
 class GameResultView(APIView):
@@ -37,3 +38,24 @@ class GetRoomNameView(APIView):
         mode = request.query_params.get("mode")
         room_name = generate_room_name(mode)
         return Response({"room_name": room_name})
+
+
+class CheckNicknameView(APIView):
+    def post(self, request):
+        nickname = request.data.get("nickname")
+        room_name = request.data.get("room_name")
+
+        # 방 내 현재 닉네임 목록을 캐시에서 가져옴
+        current_nicknames = cache.get(f"{room_name}_nicknames", set())
+
+        # 닉네임 중복 검사
+        if nickname in current_nicknames:
+            # 중복되는 경우 False 반환
+            return Response({"valid": False})
+
+        # 중복되지 않는 경우, 닉네임 추가 및 캐시 업데이트
+        current_nicknames.add(nickname)
+        cache.set(f"{room_name}_nicknames", current_nicknames)
+
+        # 사용 가능한 경우 True 반환
+        return Response({"valid": True})
